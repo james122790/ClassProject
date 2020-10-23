abb195204d0ba0a53574c5675ed56678e81f15b1
james122790 <71894283+james122790@users.noreply.github.com>
2020-10-24 00:15:23 +0100

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -2,17 +2,9 @@ function check() {
   var c=0;
   var q1=document.quiz.question1.value;
   var q2=document.quiz.question2.value;
-  var result=document.getElementById('result');
-  var quiz=document.getElementById("quiz");
+
    if (q1=="James") {c++}
    if (q2=="Casey") {c++}
  
-  
-     
-    if (c<=1) {
-       result.textContent='Your score is ${c}. Try again.
-    }
-     else{result.textContent='Your score is ${c}. You passed.
-    }
-
+document.write(c);
 }
