20203b15668f14e85ccdb5bb911f2b5f2e47c4d2
james122790 <71894283+james122790@users.noreply.github.com>
2020-10-23 23:14:39 +0100

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -2,15 +2,11 @@ function check() {
   var c=0;
   var q1=document.quiz.question1.value;
   var q2=document.quiz.question2.value;
-  var result=document.getElementById('result');
-  var quiz=document.getElementById("quiz");
+
    if (q1=="A"){c++}
    if (q2=="B"){c++}
  
   
      
-     if(c<=3){
-      result.textContent='Your result is${c}. You have failed my test.'
-  }  else {
-      result.textContent='Your result is${c}. You have passed my test.'
-  }
+     document.write(c);
+}
